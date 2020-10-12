c4576a4937103b442f690f1a04d012265bc9da02
sxnan <suxuannan95@gmail.com>
2020-10-12 15:42:28 +0800

    Introduce CacheQueryOperation

---
+++ flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/operations/CacheQueryOperation.java
@@ -0,0 +1,44 @@
+package org.apache.flink.table.operations;
+
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.util.AbstractID;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CacheQueryOperation implements QueryOperation {
+
+	private final QueryOperation child;
+	private final AbstractID intermediateDataSetId;
+
+	public CacheQueryOperation(QueryOperation child,
+							   AbstractID intermediateDataSetId) {
+		this.child = child;
+		this.intermediateDataSetId = intermediateDataSetId;
+	}
+
+	@Override
+	public TableSchema getTableSchema() {
+		return child.getTableSchema();
+	}
+
+	@Override
+	public List<QueryOperation> getChildren() {
+		return Collections.singletonList(child);
+	}
+
+	@Override
+	public String asSummaryString() {
+		Map<String, Object> args = new LinkedHashMap<>();
+		args.put("intermediateDataSetId", intermediateDataSetId);
+
+		return OperationUtils.formatWithChildren("Cache", args, getChildren(), Operation::asSummaryString);
+	}
+
+	@Override
+	public <T> T accept(QueryOperationVisitor<T> visitor) {
+		return visitor.visit(this);
+	}
+}
