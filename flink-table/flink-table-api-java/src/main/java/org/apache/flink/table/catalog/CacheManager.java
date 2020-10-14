4777164795f609fc53442451b7d4254c63999b1c
sxnan <suxuannan95@gmail.com>
2020-10-14 11:32:16 +0800

    Refactor CacheManager

--- flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/catalog/CacheManager.java
+++ flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/catalog/CacheManager.java
@@ -1,26 +1,32 @@
 package org.apache.flink.table.catalog;
 
 import org.apache.flink.annotation.Internal;
-import org.apache.flink.api.common.ClusterPartitionDescriptor;
+import org.apache.flink.api.common.PersistedIntermediateResultDescriptor;
 import org.apache.flink.table.operations.CacheQueryOperation;
 import org.apache.flink.table.operations.QueryOperation;
+import org.apache.flink.util.AbstractID;
 import org.apache.flink.util.Preconditions;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 @Internal
 public class CacheManager {
 
-	Set<CacheQueryOperation> tableToCache = new HashSet<>();
-	Map<CacheQueryOperation, Collection<ClusterPartitionDescriptor>> cachedTable = new HashMap<>();
+	Map<AbstractID, CacheQueryOperation> tableToCache = new HashMap<>();
+	Map<CacheQueryOperation, PersistedIntermediateResultDescriptor> cachedTable = new HashMap<>();
 
 	public void addTableToCache(QueryOperation queryOperation) {
 		Preconditions.checkState(queryOperation instanceof CacheQueryOperation);
-		tableToCache.add((CacheQueryOperation) queryOperation);
+
+		final CacheQueryOperation cacheQueryOperation = (CacheQueryOperation) queryOperation;
+		tableToCache.put(cacheQueryOperation.getIntermediateDataSetId(), cacheQueryOperation);
+	}
+
+	public void markTableCached(AbstractID intermediateDataSetID,
+								PersistedIntermediateResultDescriptor persistedIntermediateResultDescriptor) {
+		final CacheQueryOperation cachedQueryOperation = tableToCache.remove(new AbstractID(intermediateDataSetID));
+		cachedTable.put(cachedQueryOperation, persistedIntermediateResultDescriptor);
 	}
 
 	public boolean isCached(CacheQueryOperation cacheQueryOperation) {
