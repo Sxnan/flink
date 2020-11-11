f6d90996c89ce66d61cbe2c8948f8dd52ee904c2
sxnan <suxuannan95@gmail.com>
2020-11-11 14:17:51 +0800

    Add azure pipeline that run until fail

---
+++ tools/azure-pipelines/run_until_fail.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# This scripts runs a given test in a loop until it failes.
+# All the logs are redirected to a log file and will be deleted if the test has passed.
+
+if [ $# -ne 2 ];then
+	echo "USAGE: run_until_fail.sh TEST_DIR TEST_CLASS"
+	exit
+fi
+
+test_dir=$1
+test_class=$2
+
+echo "running test in $test_dir $test_class"
+cd $1
+
+success=0
+round=0
+
+mkdir $FLINK_ARTIFACT_DIR
+log=$FLINK_ARTIFACT_DIR/test_log_$test_class
+echo "log will be written to dir $log"
+
+mvn clean compile
+while [ $success == 0 ]
+do
+	echo "Running tests: Run $round"
+	mvn test -Dtest=$2 2>&1 > $log
+	success=$?
+	if [ $success == 0 ]; then
+		rm $log
+	else
+		echo "\nTest finished with return code $success."
+		echo "Check log at $log."
+	fi
+	round=$(($round + 1))
+done;
+
