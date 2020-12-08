093d7f66ba0c144894ba8ab74951fac08808b9ba
sxnan <suxuannan95@gmail.com>
2020-12-08 12:06:44 +0800

    Add WatermarkAlignedSplitEnumerator

---
+++ flink-connectors/flink-connector-base/src/main/java/org/apache/flink/connector/base/source/splitenumerator/WatermarkAlignedSplitEnumerator.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.connector.base.source.splitenumerator;
+
+import org.apache.flink.api.connector.source.SourceEvent;
+import org.apache.flink.api.connector.source.SourceSplit;
+import org.apache.flink.api.connector.source.SplitEnumerator;
+import org.apache.flink.api.connector.source.SplitEnumeratorContext;
+import org.apache.flink.connector.base.source.event.GlobalWatermarkEvent;
+import org.apache.flink.connector.base.source.event.SourceReaderWatermarkEvent;
+import org.apache.flink.connector.base.source.reader.WatermarkAlignedSourceReaderBase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * The WatermarkAlignedSplitEnumerator will handle {@link SourceReaderWatermarkEvent} sent by the
+ * {@link WatermarkAlignedSourceReaderBase}. It will send the {@link GlobalWatermarkEvent} to each
+ * SourceReader periodically.
+ */
+public abstract class WatermarkAlignedSplitEnumerator<SplitT extends SourceSplit, CheckpointT>
+	implements SplitEnumerator<SplitT, CheckpointT> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(WatermarkAlignedSplitEnumerator.class);
+	private final SplitEnumeratorContext<SplitT> context;
+	private final Map<Integer, Long> sourceReaderWatermarks = new HashMap<>();
+	private final Set<Integer> subtaskIds = new HashSet<>();
+	private final long period;
+
+	public WatermarkAlignedSplitEnumerator(SplitEnumeratorContext<SplitT> context, long period) {
+		this.context = context;
+		this.period = period;
+	}
+
+	@Override
+	public void start() {
+		context.callAsync(this::getGlobalWatermark, this::sendGlobalWatermark, 0, period);
+	}
+
+	@Override
+	public void addReader(int subtaskId) {
+		subtaskIds.add(subtaskId);
+	}
+
+	private void sendGlobalWatermark(Long globalWatermark, Throwable throwable) {
+		if (sourceReaderWatermarks.isEmpty()) {
+			return;
+		}
+		for (Integer subtaskId : subtaskIds) {
+			context.sendEventToSourceReader(subtaskId, new GlobalWatermarkEvent(globalWatermark));
+		}
+	}
+
+	private Long getGlobalWatermark() {
+		if (sourceReaderWatermarks.isEmpty()) {
+			return Long.MIN_VALUE;
+		}
+		return Collections.min(sourceReaderWatermarks.values());
+	}
+
+	@Override
+	public void handleSourceEvent(int subtaskId, SourceEvent sourceEvent) {
+		if (sourceEvent instanceof SourceReaderWatermarkEvent) {
+			final Long watermark = ((SourceReaderWatermarkEvent) sourceEvent).getWatermark();
+			if (watermark == null) {
+				sourceReaderWatermarks.remove(subtaskId);
+				return;
+			}
+			sourceReaderWatermarks.put(subtaskId, watermark);
+		} else {
+			LOG.warn("Receive a unknown event {}", sourceEvent);
+		}
+	}
+}
---
+++ flink-connectors/flink-connector-base/src/test/java/org/apache/flink/connector/base/source/splitenumerator/WatermarkAlignedSplitEnumeratorTest.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.connector.base.source.splitenumerator;
+
+import org.apache.flink.api.connector.source.SourceEvent;
+import org.apache.flink.api.connector.source.SplitEnumeratorContext;
+import org.apache.flink.api.connector.source.mocks.MockSourceSplit;
+import org.apache.flink.api.connector.source.mocks.MockSplitEnumeratorContext;
+import org.apache.flink.connector.base.source.event.GlobalWatermarkEvent;
+import org.apache.flink.connector.base.source.event.SourceReaderWatermarkEvent;
+
+import org.junit.Test;
+
+import javax.annotation.Nullable;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for {@link WatermarkAlignedSplitEnumerator}.
+ */
+public class WatermarkAlignedSplitEnumeratorTest {
+
+	@Test
+	public void testSendGlobalWatermarkEvent() throws Exception {
+		final MockSplitEnumeratorContext<MockSourceSplit> context =
+			new MockSplitEnumeratorContext<>(1);
+		final MockWatermarkAlignedSplitEnumerator splitEnumerator =
+			new MockWatermarkAlignedSplitEnumerator(context, 1);
+		splitEnumerator.start();
+		splitEnumerator.addReader(0);
+		splitEnumerator.handleSourceEvent(0, new SourceReaderWatermarkEvent(100L));
+		final List<Callable<Future<?>>> periodicCallables = context.getPeriodicCallables();
+		assertEquals(1, periodicCallables.size());
+		periodicCallables.get(0).call().get();
+		final List<SourceEvent> sourceEvents = context.getSentSourceEvent().get(0);
+		assertEquals(1, sourceEvents.size());
+		final SourceEvent sourceEvent = sourceEvents.get(0);
+		assertTrue(sourceEvent instanceof GlobalWatermarkEvent);
+		assertEquals(100L,
+			((GlobalWatermarkEvent) sourceEvent).getGlobalWatermark().longValue());
+	}
+
+	static class MockWatermarkAlignedSplitEnumerator
+		extends WatermarkAlignedSplitEnumerator<MockSourceSplit, Integer> {
+
+		public MockWatermarkAlignedSplitEnumerator(
+			SplitEnumeratorContext<MockSourceSplit> context,
+			long period) {
+			super(context, period);
+		}
+
+		@Override
+		public void handleSplitRequest(int subtaskId, @Nullable String requesterHostname) {
+			// do nothing
+		}
+
+		@Override
+		public void addSplitsBack(List<MockSourceSplit> splits, int subtaskId) {
+			// do nothing
+		}
+
+		@Override
+		public Integer snapshotState() throws Exception {
+			return null;
+		}
+
+		@Override
+		public void close() throws IOException {
+
+		}
+	}
+}
